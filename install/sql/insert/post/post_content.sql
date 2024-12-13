3d961c7f36c57db4a9ea897271ba337dec94238b
Fajar Riza Fauzi <fajarizaf@gmail.com>
2024-12-13 09:49:49 +0700

    update editor

--- install/sql/insert/post/post_content.sql
+++ install/sql/insert/post/post_content.sql
@@ -2,39 +2,39 @@ INSERT INTO `post_content` VALUES
 
             
             
-(1,1,'Contoh postingan blog pertama','contoh-postingan-pertama',
+(1,1,'Strategi Digital Marketing yang Efektif untuk Meningkatkan Brand Awareness','contoh-postingan-pertama',
 '<p>
 	Konten ini haya bersifat contoh, anda dapat merubahnya sesuai dengan kebutuhan konten yang ingin anda rancang:
 </p> 
 '
-, 'Konten ini hanya contoh', 'Postingan',''),
+, 'Landing page dirancang khusus untuk mendorong pengunjung melakukan tindakan tertentu', 'Postingan',''),
             
 
 
-(2,1,'Contoh postingan blog kedua','contoh-postingan-pertama',
+(2,1,'Tips Perancangan Landing page yang baik dan benar','contoh-postingan-pertama',
 '<p>
 	Konten ini haya bersifat contoh, anda dapat merubahnya sesuai dengan kebutuhan konten yang ingin anda rancang:
 </p> 
 '
-, 'Konten ini hanya contoh', 'Postingan Blog',''),
+, 'Landing page dirancang khusus untuk mendorong pengunjung melakukan tindakan tertentu', 'Postingan Blog',''),
             
             
             
-(3,1,'Contoh postingan blog ketiga','contoh-postingan-pertama',
+(3,1,'Tahapan membuat funnel marketing yang efektif untuk meningkatkan konversi','contoh-postingan-pertama',
 '<p>
 	Konten ini haya bersifat contoh, anda dapat merubahnya sesuai dengan kebutuhan konten yang ingin anda rancang:
 </p> 
 '
-, 'Konten ini hanya contoh', 'Postingan Blog',''),
+, 'Landing page dirancang khusus untuk mendorong pengunjung melakukan tindakan tertentu', 'Postingan Blog',''),
 
             
             
-(4,1,'Contoh postingan blog pertama','contoh-postingan-pertama',
+(4,1,'Peranan Landing Page Untuk Strategi Pemasaran Digital di Era Saat Ini','contoh-postingan-pertama',
 '<p>
 	Konten ini haya bersifat contoh, anda dapat merubahnya sesuai dengan kebutuhan konten yang ingin anda rancang:
 </p> 
 '
-, 'Konten ini hanya contoh', 'Postingan Blog',''),
+, 'Landing page dirancang khusus untuk mendorong pengunjung melakukan tindakan tertentu', 'Postingan Blog',''),
          
 (5,1,'Tentang Kami','tentang-kami','Contoh Halaman Tentang Kami', '','',''),
 (6,1,'Produk','produk','Contoh Halaman Produk', '','',''),
